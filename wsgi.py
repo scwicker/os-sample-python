a20369a59d06613e1e4f89856e3d159913f7360e
scwicker <31528158+scwicker@users.noreply.github.com>
2022-03-02 12:57:00 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@app.route('/post/<int:post_id>')
+@application.route("/post/<int:post_id>")
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return f'Post {post_id}'
