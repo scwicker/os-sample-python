53fa290fb34a7cdde59164eb9d8e25445d7a8825
scwicker <31528158+scwicker@users.noreply.github.com>
2022-03-09 22:18:57 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -21,15 +21,17 @@ def hello():
     return "Hello World!"
     logging.warning('hello world called!')
 
-@application.route("/post/<post_id>")
-def show_post(post_id):
+@application.route("/post/", methods=['POST'])
+def show_post():
     logging.warning(f'post id called, id: {post_id}')
     # show the post with the given id, the id is an integer
     
     #This is where we would actually do our API call to the Errata tool to check if status is SHIPPED_LIVE
     #but for proof of concept, we are calling jira back for now to close ticket.
-    callJiraBack(post_id)
-    return f'Post {post_id}'
+    #callJiraBack(post_id)
+    
+    #return f'Post {post_id}'
+    return request.json
 
 if __name__ == "__main__":
     application.run()
